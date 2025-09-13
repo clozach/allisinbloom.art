4e3a7d732d4249db49cf4a8b4f7760c8da7a09a2
Chris Lozac'h <clozach@gmail.com>
2025-09-13 16:08:37 -0700

    Duplicated sig up into by line

--- src/lib/components/Author.svelte
+++ src/lib/components/Author.svelte
@@ -1,19 +1,29 @@
-<script>
-  /**
-   * @typedef {Object} AuthorProps
-   * @property {string} name - The author's name
-   */
-</script>
-
-<span class="author">by <slot></slot></span>
+<span class="author"
+	><span class="by">by</span><img
+		class="sig"
+		src="/artist-sig.png"
+		alt="Allisin Bloom signature"
+	/></span
+>
 
 <style>
-  .author {
-    display: block;
-    font-family: 'Noto Sans', sans-serif;
-    font-weight: 100;
-    font-size: smaller;
-    margin-bottom: 1.5rem;
-    color: #555;
-  }
+	.author {
+		display: inline-flex;
+		align-items: baseline;
+		gap: clamp(.1em, 2vw, 0.5rem);
+		font-family: 'Noto Sans', sans-serif;
+		font-weight: 100;
+		margin-bottom: 1.5rem;
+		color: #555;
+	}
+
+	.by {
+		line-height: 1;
+		font-size: clamp(0.09rem, calc(0.09rem + (1.5rem - 0.09rem) * ((100vw - 50px) / (568px - 50px))), 1.5rem);
+	}
+
+	.sig {
+		width: clamp(1rem, 25vw, 9rem);
+    transform: translateY(.1em);
+	}
 </style>
